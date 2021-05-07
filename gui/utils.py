53734dd2b8a2af69cdc882ec696bfac9dbc016fc
janaSunrise <warriordefenderz@gmail.com>
2021-05-07 13:53:56 +0530

    Added predict method.

---
+++ gui/utils.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+model = tf.keras.models.load_model("models/model.h5")
+
+
+def predict(grid):
+    normalized_digit  = tf.keras.utils.normalize(grid.reshape((1, 28, 28, 1)))
+    prediction_array = model.predict(normalized_digit)
+
+    return prediction_array.argmax(), prediction_array
