61feffc6fa17bfd9d7e82a36131be5f0f669cb51
janaSunrise <warriordefenderz@gmail.com>
2021-05-08 12:16:08 +0530

    Fix the bug disallowing to predict correctly.

--- gui/utils.py
+++ gui/utils.py
@@ -4,8 +4,8 @@ model = tf.keras.models.load_model("models/model.h5")
 
 
 def predict(grid):
-    normalized_digit = tf.keras.utils.normalize(grid.reshape((1, 28, 28, 1)))
-    normalized_digit = normalized_digit / 255.0
-    prediction_array = model.predict(normalized_digit)
+    normalized_digit = grid.reshape((1, 28, 28, 1))
+    normalized_digit = normalized_digit.astype('float32') / 255.0
+    prediction_array = model.predict([normalized_digit])
 
     return prediction_array.argmax(), prediction_array
