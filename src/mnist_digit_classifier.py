d8bb71e20a6abf6fc67efb99d2b9eaa9a2ae91db
janaSunrise <warriordefenderz@gmail.com>
2021-07-15 20:02:51 +0530

    Lint the project.

--- src/mnist_digit_classifier.py
+++ src/mnist_digit_classifier.py
@@ -19,7 +19,7 @@ print("Test images", test_images.shape)
 plt.figure(figsize=(10, 10))
 
 for i in range(25):
-    plt.subplot(5,5,i+1)
+    plt.subplot(5, 5, i + 1)
 
     plt.xticks([])
     plt.yticks([])
@@ -37,8 +37,8 @@ test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
 
 # Convert into float for Keras API
-train_images = train_images.astype('float32')
-test_images = test_images.astype('float32')
+train_images = train_images.astype("float32")
+test_images = test_images.astype("float32")
 
 # Normalize the data
 train_images, test_images = train_images / 255.0, test_images / 255.0
@@ -46,38 +46,49 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 # Validation set
 validation_samples = 10000
 
+# Create validation sets and use rest for the training
 x_val = train_images[-validation_samples:]
 y_val = train_labels[-validation_samples:]
+
 train_images = train_images[:-validation_samples]
 train_labels = train_labels[:-validation_samples]
 
 # Create the model!
 model = keras.models.Sequential([
-  keras.layers.Conv2D(32, kernel_size=(3,3), input_shape=input_shape, activation="relu"),
-  keras.layers.BatchNormalization(),
-  keras.layers.MaxPooling2D(pool_size=(2, 2)),
-  keras.layers.Dropout(0.2),
+    keras.layers.Conv2D(
+    	32, kernel_size=(3, 3), input_shape=input_shape, activation="relu"
+	),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Dropout(0.2),
 
-  keras.layers.Conv2D(64, (3,3), activation="relu"),
-  keras.layers.BatchNormalization(),
-  keras.layers.MaxPooling2D(pool_size=(2, 2)),
-  keras.layers.Dropout(0.2),
+    keras.layers.Conv2D(64, (3, 3), activation="relu"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Dropout(0.2),
 
-  keras.layers.Flatten(),
-  keras.layers.Dense(128, activation="relu"),
-  keras.layers.Dropout(0.5),
-
-  keras.layers.Dense(10, activation="softmax")
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation="softmax"),
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(
+    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+)
 
 # Initializing a callback, So the model doesn't train further, If there is no proper improvement in it. Within 3 epochs.
-callback = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+callback = keras.callbacks.EarlyStopping(monitor="loss", patience=3)
 
 # We'll start the training process now, with 20 epochs.
-model.fit(train_images, train_labels, epochs=20, callbacks=[callback], validation_data=(x_val, y_val))
+model.fit(
+    train_images,
+    train_labels,
+    epochs=20,
+    callbacks=[callback],
+    validation_data=(x_val, y_val),
+)
 
 # Time to evaluate the model's predictions.
 model.evaluate(test_images, test_labels)
@@ -85,8 +96,8 @@ model.evaluate(test_images, test_labels)
 # Great, we got 98% + Of accuracy! Let's test on a random digit.
 image_index = random.randint(0, 10000)
 
-plt.imshow(test_images[image_index].reshape(28, 28),cmap="gray")
-predict = test_images[image_index].reshape(28,28)
+plt.imshow(test_images[image_index].reshape(28, 28), cmap="gray")
+predict = test_images[image_index].reshape(28, 28)
 
 pred = model.predict(test_images[image_index].reshape(1, 28, 28, 1))
 
